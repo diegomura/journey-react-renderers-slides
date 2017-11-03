bb76e0cad794289e0b9640fab5dfc7138d6cc626
Diego Muracciole <diego.muracciole@moove-it.com>
2017-11-03 09:57:18 -0300

    Build bundle with hash

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle_[hash].js'
   },
   module: {
     rules: [
