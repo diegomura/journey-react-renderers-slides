a6885edfc746705760cd6c0fceb1745722cea183
Diego Muracciole <diegomuracciole@gmail.com>
2017-11-21 01:40:33 -0300

    One React to rule them all

--- src/slides/Slide02.js
+++ src/slides/Slide02.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Slide02 = () => (
   <section data-bespoke-backdrop="middle-earth-muted">
-    <h1>React beyond the web</h1>
+    <h1 className="bullet">
+      React beyond the web
+      <span
+        className="p1 absolute flex justify-center items-center fs-18 lh-29 bullet bg-red circle"
+        style={{ bottom: -145, width: 232, height: 232, right: -70, transform: 'rotate(11deg)' }}
+      >
+        One React to rule them all!
+      </span>
+    </h1>
   </section>
 );
 
