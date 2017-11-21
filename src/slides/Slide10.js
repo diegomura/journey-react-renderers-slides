c517fe74906148323d68f1f3e46a7488480bb8ee
Diego Muracciole <diegomuracciole@gmail.com>
2017-11-21 12:36:36 -0300

    And it's fun

--- src/slides/Slide10.js
+++ src/slides/Slide10.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Slide10 = () => (
   <section data-bespoke-backdrop="middle-earth-muted">
-    <h2>Because I can</h2>
+    <h2>
+      <span className="bullet">Because I can</span>
+      <span className="bullet"> and its fun</span>
+    </h2>
   </section>
 );
 
