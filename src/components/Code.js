e4517b1f71e586e81bcc5180decde48a59380c4e
Diego Muracciole <diegomuracciole@gmail.com>
2017-11-21 02:09:05 -0300

    Fix Code component

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import classNames from 'classnames';
 
+const preCode = text => {
+  var txt = text
+    .replace(/^[\r\n]+/, "")	// strip leading newline
+    .replace(/\s+$/g, "");		// strip trailing whitespace
+
+  var mat, str, re = /^[\t ]+/gm, len, min = 1e3;
+
+  while (mat = re.exec(txt)) {
+    len = mat[0].length;
+
+    if (len < min) {
+      min = len;
+      str = mat[0];
+    }
+  }
+
+  if (min == 1e3)
+    return;
+
+  return txt.replace(new RegExp("^" + str, 'gm'), "");
+}
+
 const Code = ({ language, style, className, children }) => (
   <pre
     style={style}
     className={className}
   >
     <code className={`language-${language}`}>
-      {children.replace(/  +/g, '').trim()}
+      {preCode(children)}
     </code>
   </pre>
 );
